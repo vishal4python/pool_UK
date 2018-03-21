df6b9f35f8bb1fd1dfe5dd4e02b70292a5093c44
deepak <deepakjon31@gmail.com>
2018-03-21 18:55:51 +0530

    final data-23

--- scripts/barclays_mortgage.py
+++ scripts/barclays_mortgage.py
@@ -14,53 +14,76 @@ table = []
 table_headers = ["Bank_Product_Name", "Min_Loan_Amount", "Term (Y)", "Interest_Type", "Interest", "APRC",
                  "Mortgage_Loan_Amt"]
 # table.append(table_headers)
-browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 
+file = open("test.txt",'w')
+
+def filform(properties, deposit, year):
+    xpath = '//*[@id="mortgage-calculator-component"]/div/div[5]/form/div[2]/div/fieldset/div/div/div/div/select'
+    Select(browser.find_element_by_xpath(xpath)).select_by_value("FTBP")
+
+    time.sleep(1)
+    pxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[3]/div/fieldset/div/div/div/input"
+    # browser.find_element_by_xpath(pxpath).clear()
+    browser.find_element_by_xpath(pxpath).send_keys(properties)
+
+    repay = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[4]/div/fieldset/div/div/div/div/select"
+    Select(browser.find_element_by_xpath(repay)).select_by_value("repayment")
+
+    dxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[8]/div/fieldset/div/div/div/input"
+    browser.find_element_by_xpath(dxpath).clear()
+    browser.find_element_by_xpath(dxpath).send_keys(deposit)
+
+    yxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[11]/div/fieldset/div/div/div/div[1]/div/div/select"
+    Select(browser.find_element_by_xpath(yxpath)).select_by_value(str(year))
+
+    browser.find_element_by_css_selector(
+        "#mortgage-calculator-component > div > div.mCalc-module.mCalc-Cost > form > div:nth-child(12) > div > ul > li > button").click()
 
 def barclays(properties, deposit, year):
     browser.get("https://www.barclays.co.uk/mortgages/mortgage-calculator/#/cost")
+    browser.refresh()
 
-    time.sleep(5)
-    a = Select(browser.find_element_by_xpath('//*[@id="mortgage-calculator-component"]/div/div[5]/form/div[2]/div/fieldset/div/div/div/div/select'))
-    a.select_by_value("FTBP")
-    time.sleep(1)
-    browser.find_element_by_xpath("//*[@id='mortgage-calculator-component']/div/div[5]/form/div[3]/div/fieldset/div/div/div/input").send_keys(90000)
+    time.sleep(10)
 
-    b = Select(browser.find_element_by_xpath("//*[@id='mortgage-calculator-component']/div/div[5]/form/div[4]/div/fieldset/div/div/div/div/select"))
-    b.select_by_value("repayment")
+    try:
+        filform(properties, deposit, year)
 
-    browser.find_element_by_xpath("//*[@id='mortgage-calculator-component']/div/div[5]/form/div[8]/div/fieldset/div/div/div/input").send_keys(72000)
-
-    c = Select(browser.find_element_by_xpath("//*[@id='mortgage-calculator-component']/div/div[5]/form/div[11]/div/fieldset/div/div/div/div[1]/div/div/select"))
-    c.select_by_value("10")
-
-    browser.find_element_by_css_selector("#mortgage-calculator-component > div > div.mCalc-module.mCalc-Cost > form > div:nth-child(12) > div > ul > li > button").click()
-
+    except Exception:
+        browser.find_element_by_class_name("acsCloseButton acsAbandonButton").click()
+        filform(properties, deposit, year)
     time.sleep(15)
     # print(browser.page_source)
 
     jsoup = BeautifulSoup(browser.page_source,"html.parser")
 
     result = jsoup.find("table", attrs={"class": "table table-mortgage display responsive nowrap dataTable dtr-column collapsed"})
-    trs = result.find_all('tr')
-    for tr in trs:
-        if tr is not None:
-            tds = tr.find_all("td")
-            for td in tds:
-                product_name = re.search(r'(.*Fixed|.*Tracker)', td.text)
-                if product_name is not None:
-                    Bank_product_name = product_name.group()
-                    fixed_rate = re.search(r'\d ', Bank_product_name).group()
-                APRC = re.search(r'\d\.\d+%.*', td.text)
-                if APRC is not None:
-                    APRC = APRC.group()
-                    if len(APRC) < 6:
-                        APRC = APRC
-                    if "until" in APRC or 'BBBR' in APRC:
-                        interest = re.search(r'\d\.\d{1,}%', APRC).group()
-                if "Min loan" in td.text:
-                    max_loan = re.search(r'(Max loan) (.*\d)', td.text).groups()[1]
-                    min_loan = re.search(r'\d,\d+', td.text).group()
+    # print(result)
+    # print("---------------------------")
+    if result is not None:
+        trs = result.find_all('tr')
+        for tr in trs:
+            if tr is not None:
+                tds = tr.find_all("td")
+                for td in tds:
+                    product_name = re.search(r'(\d .*Fixed|\d .*Tracker)', td.text)
+                    if product_name is not None:
+                        Bank_product_name = product_name.group()
+                        fixed_rate = re.search(r'\d ', Bank_product_name).group()
+                        file.write((Bank_product_name+","+fixed_rate+","))
+                    APRC = re.search(r'\d\.\d+%.*', td.text)
+                    if APRC is not None:
+                        APRC = APRC.group()
+                        if len(APRC) < 6:
+                            APRC = APRC
+                            file.write(APRC+",")
+                        if "until" in APRC or 'BBBR' in APRC:
+                            interest = re.search(r'\d\.\d{1,}%', APRC).group()
+                            file.write(interest+",")
+                    if "Min loan" in td.text:
+                        max_loan = re.search(r'(Max loan) (.*\d)', td.text).groups()[1]
+                        min_loan = re.search(r'\d,\d+', td.text).group()
+                        file.write(min_loan+"\n")
 
 
     #         Interest = product.find("div", attrs={"class": "rate"}).text
@@ -83,18 +106,21 @@ def barclays(properties, deposit, year):
     #         a = [Bank_Product_Name[:Bank_Product_Name.index('%') + 1], None, year, "Fixed", Interest, APRC,
     #              str(int(properties - deposit))]
     #         table.append(a)
-        except Exception as e:
-            print(e)
+
 
 
 terms = [10, 15, 20, 25, 30]
 cases = [[90000, 72000], [270000, 216000], [450000, 360000]]
-barclays(90000, 72000, 10)
-# for term in terms:
-#     for case in cases:
-#         barclays(case[0], case[1], term)
+#barclays(90000, 72000, 10)
+for term in terms:
+    for case in cases:
+        #print(case[0], case[1], term)
+        barclays(case[0], case[1], term)
 
+
+file.close()
 browser.close()
+
 # print(tabulate(table))
 #
 # order = ["Date", "Bank_Native_Country", "State", "Bank_Name", "Bank_Local_Currency", "Bank_Type", "Bank_Product",

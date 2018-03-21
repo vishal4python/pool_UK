d04a58ba4f67d8abf7c1febacd187cde2c32e617
deepak <deepakjon31@gmail.com>
2018-03-21 23:14:20 +0530

    final code barclays mortgage

--- scripts/barclays_mortgage.py
+++ scripts/barclays_mortgage.py
@@ -6,17 +6,18 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 import pandas as pd
 import re
+import numpy as np
 from maks_lib import output_path
 from selenium.webdriver.support.ui import Select
 
 now = datetime.datetime.now()
 table = []
-table_headers = ["Bank_Product_Name", "Min_Loan_Amount", "Term (Y)", "Interest_Type", "Interest", "APRC",
-                 "Mortgage_Loan_Amt"]
+table_headers = ["Bank_Product_Name", "Fixed_Rate_Term","APRC", "Interest","Min_Loan_Amount","Mortgage_Loan_Amt","Term (Y)"]
 # table.append(table_headers)
-browser = webdriver.Firefox()
+print("Scrapping is in-progress..")
+browser = webdriver.Chrome()
 
-file = open("test.txt",'w')
+file = open("test.txt",'w',encoding="UTF-8")
 
 def filform(properties, deposit, year):
     xpath = '//*[@id="mortgage-calculator-component"]/div/div[5]/form/div[2]/div/fieldset/div/div/div/div/select'
@@ -24,8 +25,9 @@ def filform(properties, deposit, year):
 
     time.sleep(1)
     pxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[3]/div/fieldset/div/div/div/input"
-    # browser.find_element_by_xpath(pxpath).clear()
+    browser.find_element_by_xpath(pxpath).clear()
     browser.find_element_by_xpath(pxpath).send_keys(properties)
+    #browser.find_element_by_xpath(pxpath).send_keys("90000")
 
     repay = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[4]/div/fieldset/div/div/div/div/select"
     Select(browser.find_element_by_xpath(repay)).select_by_value("repayment")
@@ -33,16 +35,17 @@ def filform(properties, deposit, year):
     dxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[8]/div/fieldset/div/div/div/input"
     browser.find_element_by_xpath(dxpath).clear()
     browser.find_element_by_xpath(dxpath).send_keys(deposit)
+    #browser.find_element_by_xpath(dxpath).send_keys("72000")
 
     yxpath = "//*[@id='mortgage-calculator-component']/div/div[5]/form/div[11]/div/fieldset/div/div/div/div[1]/div/div/select"
     Select(browser.find_element_by_xpath(yxpath)).select_by_value(str(year))
+    #Select(browser.find_element_by_xpath(yxpath)).select_by_value("10")
 
-    browser.find_element_by_css_selector(
-        "#mortgage-calculator-component > div > div.mCalc-module.mCalc-Cost > form > div:nth-child(12) > div > ul > li > button").click()
+    browser.find_element_by_css_selector("#mortgage-calculator-component > div > div.mCalc-module.mCalc-Cost > form > div:nth-child(12) > div > ul > li > button").click()
 
 def barclays(properties, deposit, year):
     browser.get("https://www.barclays.co.uk/mortgages/mortgage-calculator/#/cost")
-    browser.refresh()
+    #browser.refresh()
 
     time.sleep(10)
 
@@ -50,8 +53,12 @@ def barclays(properties, deposit, year):
         filform(properties, deposit, year)
 
     except Exception:
-        browser.find_element_by_class_name("acsCloseButton acsAbandonButton").click()
-        filform(properties, deposit, year)
+        try:
+            browser.find_element_by_class_name("acsCloseButton acsAbandonButton").click()
+            filform(properties, deposit, year)
+        except Exception as e:
+            browser.close()
+            print(e)
     time.sleep(15)
     # print(browser.page_source)
 
@@ -70,6 +77,11 @@ def barclays(properties, deposit, year):
                     if product_name is not None:
                         Bank_product_name = product_name.group()
                         fixed_rate = re.search(r'\d ', Bank_product_name).group()
+                        Interest_type =  re.search(r'(Fixed|Tracker)', Bank_product_name).group()
+                        if "Fixed" in Interest_type:
+                            Interest_Type = "Fixed"
+                        if "Tracker" in Interest_type:
+                            Interest_Type = "Variable"
                         file.write((Bank_product_name+","+fixed_rate+","))
                     APRC = re.search(r'\d\.\d+%.*', td.text)
                     if APRC is not None:
@@ -83,31 +95,36 @@ def barclays(properties, deposit, year):
                     if "Min loan" in td.text:
                         max_loan = re.search(r'(Max loan) (.*\d)', td.text).groups()[1]
                         min_loan = re.search(r'\d,\d+', td.text).group()
-                        file.write(min_loan+"\n")
-
-
-    #         Interest = product.find("div", attrs={"class": "rate"}).text
-    #         APRC = product.find("div", attrs={"class": "cost-comparison"}).text
-    #
-    #         Interest = re.findall('\d.\d?\d?\d?%', Interest)
-    #         if len(Interest) >= 1:
-    #             Interest = Interest[0]
-    #         else:
-    #             Interest = None
-    #
-    #         APRC = re.findall('\d.*APRC', APRC)
-    #
-    #         if len(APRC) >= 1:
-    #             APRC = APRC[0]
-    #             APRC = re.sub('[^0-9.%]', '', APRC)
-    #         else:
-    #             APRC = None
-    #
-    #         a = [Bank_Product_Name[:Bank_Product_Name.index('%') + 1], None, year, "Fixed", Interest, APRC,
-    #              str(int(properties - deposit))]
-    #         table.append(a)
-
-
+                        min_loan = min_loan.replace(",","")
+                        #print(min_loan,"--------------------------------------")
+                        file.write(min_loan+","+str(deposit)+","+str(year)+"\n")
+def panda():
+    dataset = pd.read_table('test.txt', sep=',', delimiter=None, header=None)
+    #dataset.columns = ['Bank_Product_Name', 'Fixed_Rate_Term', 'Interest_Type', "Interest", "APRC", "Term (Y)", 'Mortgage_Loan_Amt']
+    dataset.columns = ["Bank_Product_Name", "Fixed_Rate_Term","APRC", "Interest","Min_Loan_Amount","Mortgage_Loan_Amt","Term (Y)"]
+    print(dataset["Min_Loan_Amount"])
+    dataset['Date'] = now.strftime("%Y-%m-%d")
+    dataset['Bank_Native_Country'] = "UK"
+    dataset['State'] = "London"
+    dataset['Bank_Name'] = "Barclays"
+    dataset['Bank_Local_Currency'] = "GBP"
+    dataset['Bank_Type'] = "Bank"
+    dataset['Bank_Product'] = 'Mortgages'
+    dataset['Bank_Product_Type'] = "Mortgages"
+    dataset['Bank_Offer_Feature'] = "Offline"
+    dataset['Mortgage_Down_Payment'] = "20%"
+    dataset['Mortgage_Category'] = "New Purchase"
+    dataset['Mortgage_Reason'] = "Primary Residence"
+    dataset['Mortgage_Pymt_Mode'] = "Principal + Interest"
+    dataset['Bank_Product_Code'] = np.nan
+    dataset["Interest_Type"] = np.nan
+    columns = ['Date', 'Bank_Native_Country', 'State', 'Bank_Name', 'Bank_Local_Currency', 'Bank_Type', 'Bank_Product',
+               'Bank_Product_Type', 'Bank_Product_Name', 'Min_Loan_Amount', 'Bank_Offer_Feature', 'Term (Y)',
+               'Interest_Type', 'Interest', 'APRC', 'Mortgage_Loan_Amt', 'Mortgage_Down_Payment', 'Mortgage_Category', 'Mortgage_Reason',
+               'Mortgage_Pymt_Mode', 'Fixed_Rate_Term', 'Bank_Product_Code'
+               ]
+    df = dataset.reindex(columns=columns)
+    df.to_csv(output_path + "Consolidate_Barclays_Data_Mortgage_{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
 
 terms = [10, 15, 20, 25, 30]
 cases = [[90000, 72000], [270000, 216000], [450000, 360000]]
@@ -120,35 +137,6 @@ for term in terms:
 
 file.close()
 browser.close()
-
-# print(tabulate(table))
-#
-# order = ["Date", "Bank_Native_Country", "State", "Bank_Name", "Bank_Local_Currency", "Bank_Type", "Bank_Product",
-#          "Bank_Product_Type", "Bank_Product_Name", "Min_Loan_Amount", "Bank_Offer_Feature", "Term (Y)", "Interest_Type",
-#          "Interest", "APRC", "Mortgage_Loan_Amt", "Mortgage_Down_Payment", "Mortgage_Category", "Mortgage_Reason",
-#          "Mortgage_Pymt_Mode", "Fixed_Rate_Term", "Bank_Product_Code"]
-# column = ["Bank_Product_Name", "Min_Loan_Amount", "Term (Y)", "Interest_Type", "Interest", "APRC", "Mortgage_Loan_Amt"]
-#
-# df = pd.DataFrame(table, columns=column)
-#
-# df["Date"] = now.strftime('%Y-%m-%d')
-#
-# df["Bank_Native_Country"] = "UK"
-# df["State"] = "London"
-# df["Bank_Name"] = "Lloyds Bank"
-# df["Bank_Local_Currency"] = "GBP"
-# df["Bank_Type"] = "Bank"
-# df["Bank_Product"] = "Mortgages"
-# df["Bank_Product_Type"] = "Mortgages"
-# df["Bank_Offer_Feature"] = "Offline"
-# df["Mortgage_Down_Payment"] = "20%"
-# df["Mortgage_Category"] = "New Purchase"
-# df["Mortgage_Reason"] = "Primary Residence"
-# df["Mortgage_Pymt_Mode"] = "Principal + Interest"
-# df["Bank_Product_Code"] = None
-# df["Fixed_Rate_Term"] = df["Bank_Product_Name"].apply(lambda x: re.match('\d\d?', str(x)).group(0))
-#
-# print(df)
-#
-# df = df[order]
-# df.to_csv(output_path + "Consolidate_Lloyds_Mortgage_{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
+panda()
+os.remove("test.txt")
+print("Scrapping Finished !!!")

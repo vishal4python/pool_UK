8e0fcec92fef08b9ba3824ee7426fd9f26932495
deepak <deepakjon31@gmail.com>
2018-03-26 19:40:44 +0530

    Changes:
    change on the current account

--- scripts/barclays_deposite.py
+++ scripts/barclays_deposite.py
@@ -17,7 +17,7 @@ links = ['https://www.barclays.co.uk/savings/instant-access/',
          'https://www.barclays.co.uk/savings/bonds/',
          'https://www.barclays.co.uk/current-accounts/'
          ]
-table_headers = ['Bank_Product_Name','AER','Interest','Balance','Min_opening_Bal','Bank_Product_Type']
+table_headers = ['Bank_Product_Name','AER','Interest','Balance','Min_opening_Bal','Bank_Product_Type','Term in Months']
 
 def instant_access():
     res = requests.get("https://www.barclays.co.uk/savings/instant-access/")
@@ -35,15 +35,15 @@ def instant_access():
         else:
             product_name = p[0].text.replace("\n","")
             if c == 1:
-                AER1 = re.search(r'\d+%',p[1].text).group()
+                AER1 = re.search(r'\d\.\d+%',p[1].text).group()
                 interest1 = AER1
                 Balance1 = re.search(r'\(.*\)',p[1].text).group().rstrip(")").lstrip("(")
-                AER2 = re.search(r'\d+%',p[2].text).group()
+                AER2 = re.search(r'\d\.\d+%',p[2].text).group()
                 interest2 = AER2
                 Balance2 = re.search(r'\(.*\)',p[2].text).group().rstrip(")").lstrip("(")
                 MIN = p[3].text
-                data.append([product_name, AER1,interest1,Balance1, MIN, bank_type])
-                data.append([product_name, AER2, interest2,Balance2, MIN, bank_type])
+                data.append([product_name, AER1,interest1,Balance1, MIN, bank_type,None])
+                data.append([product_name, AER2, interest2,Balance2, MIN, bank_type,None])
                 c += 1
 
 def iaas():
@@ -68,15 +68,16 @@ def iaas():
                 interest3 = AER3 = p[8].text
                 Balance3 = p[9].text.lstrip("(").rstrip(")2")
                 MIN = p[10].text.strip("2")
-                data.append([product_name, AER1, interest1, Balance1, MIN,'Savings'])
-                data.append([product_name, AER2, interest2, Balance2, MIN,'Savings'])
-                data.append([product_name, AER3, interest3, Balance3, MIN,'Savings'])
+                data.append([product_name, AER1, interest1, Balance1, MIN,'Savings',None])
+                data.append([product_name, AER2, interest2, Balance2, MIN,'Savings',None])
+                data.append([product_name, AER3, interest3, Balance3, MIN,'Savings',None])
                 c += 1
             elif c == 2:
                 interest = AER = re.search(r'\d\.\d+%',p[1].text).group()
                 Balance = p[2].text.strip("(").strip(")2")
                 MIN = p[3].text.strip("2")
-                data.append([product_name, AER, interest, Balance, MIN,'Savings'])
+                Term_in_Months = int(product_name.split("-")[0]) * 12
+                data.append([product_name, AER, interest, Balance, MIN,'Savings',Term_in_Months])
                 c += 1
 
 def bonds():
@@ -92,17 +93,18 @@ def bonds():
             continue
         else:
             product_name = p[0].text.replace("\n", "")
+            Term_in_Months = int(product_name.split("-")[0]) * 12
             if c == 1:
                 interest = AER = p[1].text
                 Balance = p[2].text.strip("(").strip(")\n")
                 MIN = p[3].text
-                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits'])
+                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits',Term_in_Months])
                 c += 1
             elif c == 2:
                 interest = AER = p[1].text
                 Balance = p[2].text.strip("(").strip(")\n")
                 MIN = p[3].text.rstrip("\n")
-                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits'])
+                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits',Term_in_Months])
                 c += 1
 
 def current():
@@ -111,8 +113,8 @@ def current():
     results = soup.find_all("h3", attrs={"class": "promo-title"})
     product_name1 = results[0].text
     product_name2 = results[1].text
-    data.append([product_name1, None, None, None, None, None])
-    data.append([product_name2, None, None, None, None, None])
+    data.append([product_name1, None, None, None, None, "Current",None])
+    data.append([product_name2, None, None, None, None, "Current",None])
 
 def main():
     instant_access()
@@ -133,13 +135,13 @@ if __name__ == '__main__':
     data['Bank_Type'] = 'Bank'
     data['Bank_Product'] = 'Deposits'
     data['Bank_Offer_Feature'] = "Offline"
-    data["Term in Months"] = np.nan
+    #data["Term in Months"] = np.nan
     data["Bank_Product_Code"] = np.nan
-    data["Interest_Type"] = "Variable"
+    data["Interest_Type"] = "Fixed"
     data["Min_opening_Bal"] = data["Min_opening_Bal"].apply(lambda x: re.sub('[^0-9.-]','',str(x)))
     data["Balance"] = " "+data["Balance"].apply(lambda x: re.sub('[^0-9.,-]','',str(x).replace('to','-').replace("million", "000000")))
     order = ['Date', 'Bank_Native_Country', 'State', 'Bank_Name', 'Bank_Local_Currency', 'Bank_Type', 'Bank_Product',
-             'Bank_Product_Type', 'Bank_Product_Name','Min_opening_Bal','Balance', 'Bank_Offer_Feature', 'Term in Months', 'Interest_Type',
+             'Bank_Product_Type', 'Bank_Product_Name','Balance', 'Bank_Offer_Feature', 'Term in Months', 'Interest_Type',
              'Interest', 'AER', 'Bank_Product_Code']
     df = data[order]
     df.to_csv(path, index=False)

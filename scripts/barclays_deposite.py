83bc6d19a07d09f29323ae2cd0621838eb98c6db
deepak <deepakjon31@gmail.com>
2018-03-26 18:19:31 +0530

    final code for barclays deposit

--- scripts/barclays_deposite.py
+++ scripts/barclays_deposite.py
@@ -3,7 +3,15 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 from tabulate import tabulate
+import numpy as np
+import datetime
+now = datetime.datetime.now()
+from maks_lib import output_path
+import warnings
+warnings.simplefilter(action='ignore')
 data = []
+path = output_path + "Consolidate_Barclays_Data_Deposit_{}.csv".format(now.strftime("%m_%d_%Y"))
+
 links = ['https://www.barclays.co.uk/savings/instant-access/',
          'https://www.barclays.co.uk/savings/isas/',
          'https://www.barclays.co.uk/savings/bonds/',
@@ -37,20 +45,6 @@ def instant_access():
                 data.append([product_name, AER1,interest1,Balance1, MIN, bank_type])
                 data.append([product_name, AER2, interest2,Balance2, MIN, bank_type])
                 c += 1
-            elif c == 2:
-                product_name2 = p[2].text.replace("\n","")
-                AER = re.search(r'\d+\.\d+% AER',p[3].text).group().rstrip("AER")
-                interest = re.search(r'\d\.\d+%$', p[3].text).group()
-                if "£" in p[4].text:
-                    MIN = p[4].text.replace("\n","")
-                    data.append(([product_name, AER, interest, None, MIN, bank_type]))
-                    data.append(([product_name2, AER, interest, None, MIN, bank_type]))
-                else:
-                    MIN = re.search(r'£\d+',p[8].text).group()
-                    AER2 = re.search(r'\d+\.\d+% AER',p[6].text).group().rstrip("AER")
-                    interest2 =  re.search(r'\d\.\d+%$', p[6].text).group()
-                    data.append(([product_name,AER,interest, None, MIN, bank_type]))
-                    data.append(([product_name2,AER2,interest2, None, MIN, bank_type]))
 
 def iaas():
     res = requests.get('https://www.barclays.co.uk/savings/isas/')
@@ -84,25 +78,69 @@ def iaas():
                 MIN = p[3].text.strip("2")
                 data.append([product_name, AER, interest, Balance, MIN,'Savings'])
                 c += 1
-            elif c == 3:
-                product_name1 = product_name + " "+p[2].text
-                interest = AER = re.search(r'\d.\d+%',p[3].text).group()
-                Balance = p[4].text.lstrip("(").rstrip(")2")
-                MIN = p[5].text.strip("2")
-                data.append([product_name1, AER, interest, Balance, MIN,'Savings'])
+
+def bonds():
+    res = requests.get('https://www.barclays.co.uk/savings/bonds/')
+    soup = bs(res.content, "html.parser")
+    result = soup.find("table", attrs={"id": "table"})
+    trs = result.find_all('tr')
+    c = 0
+    for tr in trs:
+        p = tr.find_all('p')
+        if c == 0:
+            c += 1
+            continue
+        else:
+            product_name = p[0].text.replace("\n", "")
+            if c == 1:
+                interest = AER = p[1].text
+                Balance = p[2].text.strip("(").strip(")\n")
+                MIN = p[3].text
+                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits'])
                 c += 1
-            elif c == 4:
-                product_name1 = product_name +' '+ p[2].text
-                AER = re.search(r'\d+\.\d+% AER', p[3].text).group().rstrip("AER")
-                interest = re.search(r'\d\.\d+% variable', p[3].text).group().strip("variable")
-                data.append([product_name1, AER, interest, None, None,'Savings'])
+            elif c == 2:
+                interest = AER = p[1].text
+                Balance = p[2].text.strip("(").strip(")\n")
+                MIN = p[3].text.rstrip("\n")
+                data.append([product_name, AER, interest, Balance, MIN, 'Term Deposits'])
                 c += 1
+
+def current():
+    res = requests.get('https://www.barclays.co.uk/current-accounts/')
+    soup = bs(res.content, "html.parser")
+    results = soup.find_all("h3", attrs={"class": "promo-title"})
+    product_name1 = results[0].text
+    product_name2 = results[1].text
+    data.append([product_name1, None, None, None, None, None])
+    data.append([product_name2, None, None, None, None, None])
 
 def main():
     instant_access()
     iaas()
-    print(tabulate(data))
-    print("Not Implemented completely")
+    bonds()
+    current()
+    #print(tabulate(data))
 
 if __name__ == '__main__':
+    print("Scraping Started..")
     main()
+    data = pd.DataFrame(data, columns=table_headers)
+    data['Date'] = now.strftime("%Y-%m-%d")
+    data['Bank_Native_Country'] = 'UK'
+    data['State'] = 'London'
+    data['Bank_Name'] = 'Barclays Bank'
+    data['Bank_Local_Currency'] = 'GBP'
+    data['Bank_Type'] = 'Bank'
+    data['Bank_Product'] = 'Deposits'
+    data['Bank_Offer_Feature'] = "Offline"
+    data["Term in Months"] = np.nan
+    data["Bank_Product_Code"] = np.nan
+    data["Interest_Type"] = "Variable"
+    data["Min_opening_Bal"] = data["Min_opening_Bal"].apply(lambda x: re.sub('[^0-9.-]','',str(x)))
+    data["Balance"] = " "+data["Balance"].apply(lambda x: re.sub('[^0-9.,-]','',str(x).replace('to','-').replace("million", "000000")))
+    order = ['Date', 'Bank_Native_Country', 'State', 'Bank_Name', 'Bank_Local_Currency', 'Bank_Type', 'Bank_Product',
+             'Bank_Product_Type', 'Bank_Product_Name','Min_opening_Bal','Balance', 'Bank_Offer_Feature', 'Term in Months', 'Interest_Type',
+             'Interest', 'AER', 'Bank_Product_Code']
+    df = data[order]
+    df.to_csv(path, index=False)
+    print("Scraping Finished !!!")

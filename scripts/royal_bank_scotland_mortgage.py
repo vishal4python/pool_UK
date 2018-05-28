afdcd8f36a2d61f0501488a16264469ef14113f7
dsairam143 <37208282+dsairam143@users.noreply.github.com>
2018-05-28 18:33:09 +0530

    Update royal_bank_scotland_mortgage.py

--- scripts/royal_bank_scotland_mortgage.py
+++ scripts/royal_bank_scotland_mortgage.py
@@ -4,11 +4,16 @@ Created on 14-Mar-2018
 @author: sairam
 '''
 import re
-import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import datetime
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+starttime = time.time()
 from maks_lib import output_path
 today = datetime.datetime.now()
 path = output_path+"Consolidate_RBS_Data_Mortgage_"+str(today.strftime('%Y_%m_%d'))+'.csv'
@@ -19,15 +24,25 @@ table = []
 order = ["Date", "Bank_Native_Country", "State", "Bank_Name", "Bank_Local_Currency", "Bank_Type", "Bank_Product", "Bank_Product_Type", "Bank_Product_Name", "Min_Loan_Amount", "Bank_Offer_Feature", "Term (Y)", "Interest_Type", "Interest", "APRC", "Mortgage_Loan_Amt", "Mortgage_Down_Payment", "Mortgage_Category", "Mortgage_Reason", "Mortgage_Pymt_Mode", "Fixed_Rate_Term", "Bank_Product_Code"]
 table_headers = ["Bank_Product_Name", "Min_Loan_Amount", "Term (Y)", "Interest_Type", "Interest", "APRC", "Mortgage_Loan_Amt", "Fixed_Rate_Term", "Mortgage_Down_Payment"]
 driver.get("https://personal.rbs.co.uk/personal/mortgages/mortgage-calculators/mortgagefinder.html")
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/div[5]/div/a').click()
+time.sleep(5)
+try:
+    driver.find_element_by_xpath('/html/body/div[5]/div/a').click()
+except:
+    pass
 # driver.find_element_by_css_selector("#mortgageFinder_mortgage-term").send_keys("20")
 cases = [[90000,18000],[270000,54000], [450000,90000]]
 for case in cases:
     terms = [10,15,20,30]
     for term in terms:
-        driver.find_element_by_xpath('//*[@id="mortgageFinder_mortgage-term"]').clear()
-        driver.find_element_by_xpath('//*[@id="mortgageFinder_mortgage-term"]').send_keys(term)
+        # driver.find_element_by_xpath('//*[@id="mortgageFinder_mortgage-term"]').clear()
+        # driver.find_element_by_xpath('//*[@id="mortgageFinder_mortgage-term"]').send_keys(term)
+
+        WebDriverWait(driver, 10000).until(EC.visibility_of_element_located((By.NAME, 'mortgage-term')))
+        date_element = driver.find_element_by_name('mortgage-term')
+        date_element.click()
+        date_element.send_keys(Keys.BACK_SPACE, Keys.BACK_SPACE)
+        date_element.send_keys(term)
+
         driver.find_element_by_name("PropertyValue").clear()
         driver.find_element_by_name("PropertyValue").send_keys(case[0])
         # time.sleep(2)
@@ -36,7 +51,7 @@ for case in cases:
         time.sleep(2)
         driver.find_element_by_tag_name("body").click()
         # driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/div[3]/div/div/form/div/fieldset/div[1]/div[3]/div/div[11]/div/span/a').click()
-        driver.find_element_by_css_selector(".js-cta-button > a:nth-child(1)").click()
+        driver.find_element_by_css_selector("span.cta:nth-child(1)").click()   #span.cta:nth-child(1)
         time.sleep(5)
         # print(driver.page_source)
         jsoup = BeautifulSoup(driver.page_source,"lxml")
@@ -96,3 +111,4 @@ df = df[order]
 df.to_csv(path, index=False)
 driver.close()
 print("execution Completed.")
+print('time=',(time.time()-starttime))
